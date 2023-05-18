94eb9168d4f110f7a98b1d37bdfadc4128322575
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-18 09:29:44 +0530

    lengthofline

---
+++ length of line.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program on .Net");
+            int x1, x2, y1, y2;
+            double length;
+            Console.WriteLine("------------Calculating Length of the Line-------------");
+            Console.WriteLine("Enter X1 Value");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y1 Value");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter X2 Value");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Y2 Value");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of the Line is--" + length);
+
+        }
+    }
+}
